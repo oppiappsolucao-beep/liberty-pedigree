ebeb75526642ed6b873be2ba111e7ffdc3872648
oppiappsolucao-beep <oppiappsolucao@gmail.com>
2026-06-16 16:14:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Liberty Pedigree",
+    page_icon="🐶",
+    layout="wide"
+)
+
+st.title("🐶 Liberty Pedigree")
+st.write("Primeira versão do dashboard.")
+
+st.success("Dashboard online com sucesso.")
